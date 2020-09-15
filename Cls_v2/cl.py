4f9d9d905ba497a6311dfa63dbb3ed3db0f30c7b
carlosggarcia <carlosgarcia@iff.csic.es>
2020-09-15 16:52:05 +0100

    added Field class + gc noise

--- Cls_v2/cl.py
+++ Cls_v2/cl.py
@@ -1,24 +1,99 @@
 #!/usr/bin/python
+import common as co
 import numpy as np
 import healpy as hp
 import pymaster as nmt
 import os
 import yaml
 
+class Field():
+    def __init__(self, data, tr, maps=None, mask=None):
+        self.data = co.read_data(data)
+        self.tr = tr
+        self.spin = self.data['tracers'][tr]['spin']
+        self._raw_maps = None
+        self._maps = maps
+        self._mask = mask
+        self.f = self.compute_field()
+
+    def get_field(self):
+        return self.f
+
+    def compute_field(self):
+        data = self.data
+        mask = self.get_mask()
+        maps = self.get_maps()
+        f = nmt.NmtField(mask, maps, n_iter=data['healpy']['n_iter'])
+        return f
+
+    def get_mask(self):
+        if self._mask is None:
+            data = self.data
+            tracer = data['tracers'][self.tr]
+            mask = hp.read_map(tracer['mask'])
+            mask_good = mask >= tracer['threshold']
+            mask[~mask_good] = 0.
+            self._mask = mask
+        return self._mask
+
+    def get_maps(self):
+        if self._maps is None:
+            data = self.data
+            tracer = data['tracers'][self.tr]
+            mask = self.get_mask()
+            mask_good = mask >= tracer['threshold']
+            # TODO: This can be optimize for the case mask1 == mask2
+            raw_maps = self.get_raw_maps()
+            if self.spin == 0:
+                maps = np.zeros((1, mask.size))
+                raw_map = raw_maps[0]
+                mean_raw_map = raw_map[mask_good].sum() / mask[mask_good].sum()
+                map_dg = np.zeros_like(raw_map)
+                map_dg[mask_good] = raw_map[mask_good] / (mean_raw_map * mask[mask_good]) - 1
+                maps[0] = map_dg
+            else:
+                maps = np.zeros((2, mask.size))
+                sums = np.load(tracer['sums'])
+                map_we1, map_we2 = raw_maps
+                opm_mean = sums['wopm'] / sums['w']
+
+                maps[0, mask_good] = (map_we1[mask_good]/mask[mask_good]) / opm_mean
+                maps[1, mask_good] = (map_we2[mask_good]/mask[mask_good]) / opm_mean
+            self._maps = maps
+
+        return self._maps
+
+    def get_raw_maps(self):
+        tracer = self.data['tracers'][self.tr]
+        if self._raw_maps is None:
+            if self.spin == 0:
+                raw_map = hp.read_map(tracer['path'])
+                self._raw_maps = np.array([raw_map])
+            else:
+                map_we1 = hp.read_map(tracer['path1'])
+                map_we2 = hp.read_map(tracer['path2'])
+                self._raw_maps = np.array([map_we1, map_we2])
+
+        return self._raw_maps
+
+
+
 class Cl():
     def __init__(self, data, tr1, tr2):
-        self.data = self.read_data(data)
+        self._datapath = data
+        self.data = co.read_data(data)
         self.tr1 = tr1
         self.tr2 = tr2
         self.outdir = self.get_outdir()
         os.makedirs(self.outdir, exist_ok=True)
         self.b = self.get_NmtBin()
+        # Not needed to load cl if already computed
+        self._f1 = None
+        self._f2 = None
+        self._w = None
+        ##################
         self.ell, self.cl = self.get_ell_cl()
 
-    def read_data(self, data):
-        with open(data) as f:
-            data = yaml.safe_load(f)
-        return data
 
     def get_outdir(self):
         root = self.data['output']
@@ -35,26 +110,18 @@ class Cl():
         b = nmt.NmtBin.from_edges(bpw_edges[:-1], bpw_edges[1:])
         return b
 
-    def get_field(self, tr):
-        data = self.data
-        tracers = data['tracers']
-        # TODO: This can be optimize for the case mask1 == mask2
-        mask = hp.read_map(tracers[tr]['mask'])
-        maps = []
-        if tracers[tr]['spin'] == 0:
-            maps.append(hp.read_map(tracers[tr]['path']))
-        else:
-            maps.append(hp.read_map(tracers[tr]['path1']))
-            maps.append(hp.read_map(tracers[tr]['path2']))
-        f = nmt.NmtField(mask, maps, n_iter=data['healpy']['n_iter'])
-        return f
-
-    def load_fields(self):
-        self.f1 = self.get_field(self.tr1)
-        self.f2 = self.get_field(self.tr2)
-        return self.f1, self.f2
+    def get_fields(self):
+        if self._f1 is None:
+            self._f1 = Field(self._datapath, self.tr1)
+            self._f2 = Field(self._datapath, self.tr2)
+        return self._f1, self._f2
 
     def get_workspace(self):
+        if self._w is None:
+            self._w = self._compute_workspace()
+        return self._w
+
+    def _compute_workspace(self):
         mask1 = os.path.basename(self.data['tracers'][self.tr1]['mask'])
         mask2 = os.path.basename(self.data['tracers'][self.tr2]['mask'])
         # Remove the extension
@@ -64,21 +131,51 @@ class Cl():
         w = nmt.NmtWorkspace()
         if not os.path.isfile(fname):
             n_iter = self.data['healpy']['n_iter']
-            w.compute_coupling_matrix(self.f1, self.f2, self.b,
+            f1, f2 = self.get_fields()
+            w.compute_coupling_matrix(f1.f, f2.f, self.b,
                                       n_iter=n_iter)
             w.write_to(fname)
         else:
             w.read_from(fname)
         return w
 
+    def _compute_noise_gc(self):
+        map_ng = self._f1.get_raw_maps()[0]
+        map_dg = self._f1.get_maps()[0]
+        mask = self._f1.get_mask()
+        mask_good = mask > 0  # Already set to 0 all bad pixels in Field()
+        npix = mask.size
+        nside = hp.npix2nside(npix)
+
+        N_mean = map_ng[mask_good].sum() / mask[mask_good].sum()
+        N_mean_srad = N_mean / (4 * np.pi) * npix
+        N_ell = mask.sum() / npix / N_mean_srad
+        w = self.get_workspace()
+        return w.decouple_cell([N_ell * np.ones(3 * nside)])[0]
+
+    def _compute_noise_wl(self):
+        pass
+
+
+    def compute_noise(self):
+        tracers = self.data['tracers']
+        ell = self.b.get_effective_ells()
+        if self.tr1!= self.tr2:
+            return 0 * ell
+        elif tracers[self.tr1]['spin'] == 0:
+            return self._compute_noise_gc()
+        else:
+            return self._compute_noise_wl()
+
     def get_ell_cl(self):
         fname = os.path.join(self.outdir, 'cl_{}_{}.npz'.format(self.tr1, self.tr2))
         ell = self.b.get_effective_ells()
         if not os.path.isfile(fname):
-            f1, f2 = self.load_fields()
+            f1, f2 = self.get_fields()
             w = self.get_workspace()
-            cl = w.decouple_cell(nmt.compute_coupled_cell(f1, f2))
-            np.savez(fname, ell=ell, cl=cl)
+            cl = w.decouple_cell(nmt.compute_coupled_cell(f1.f, f2.f))
+            nl = self.compute_noise()
+            np.savez(fname, ell=ell, cl=cl-nl, nl=nl)
         else:
             cl_file = np.load(fname)
             cl = cl_file['cl']
